fef0e103ff7d2848abb7f654f8384bdf2f66aec6
WaterMeloon <ifkovics.barnabas@gmail.com>
2023-05-17 18:02:23 +0200

    Refactor variables

--- parser.c
+++ parser.c
@@ -30,8 +30,8 @@ bool check_extension(const char *file_path) {
     return (strcmp(extension, "ciff") == 0 || strcmp(extension, "caff") == 0);
 }
 
-char *file_name;
-char* _file_name(const char* file_path) {
+char *g_file_name;
+void set_g_file_name(const char* file_path) {
     char *separator = strrchr(file_path, '/');
     if (separator == NULL) {
         separator = (char *)file_path;
@@ -47,20 +47,20 @@ char* _file_name(const char* file_path) {
         *ext = '\0';
     }
     strcat((char *)file_name, ".jpg");
-    return file_name;
+    g_file_name = file_name;
 }
 
-void read_bytes(FILE *file, void *buff, const size_t buff_cap){
-    assert(buff_cap != 0);
-    size_t bytes_read = fread(buff, buff_cap, 1, file);
+void read_bytes_to_buffer(FILE *file, void *buffer, const size_t buffer_capacity){
+    assert(buffer_capacity != 0);
+    size_t bytes_read = fread(buffer, buffer_capacity, 1, file);
     if (bytes_read != 1) {
         if (ferror(file)) {
             fprintf(stderr, "%sERROR%s: could not read %zu bytes from file: %s\n",
-                    ERR_SET, RESET, buff_cap, strerror(errno));
+                    ERR_SET, RESET, buffer_capacity, strerror(errno));
             exit(-1);
         } else if (feof(file)) {
             fprintf(stderr, "%sERROR%s: could not read %zu bytes from file: end of file\n",
-                    ERR_SET, RESET, buff_cap);
+                    ERR_SET, RESET, buffer_capacity);
             exit(-1);
         } else {
             assert(0 && "unreachable");
@@ -68,34 +68,34 @@ void read_bytes(FILE *file, void *buff, const size_t buff_cap){
     }
 }
 
-size_t translate_bytes(uint8_t *buff, const size_t buff_cap){
-    uint64_t size = 0;
-    for (int i = 0; i < buff_cap; ++i) {
-        size |= (uint64_t)buff[i] << (i * 8);
+size_t translate_bytes(uint8_t *buffer, const size_t buffer_capacity){
+    uint64_t value = 0;
+    for (unsigned int i = 0; i < buffer_capacity; ++i) {
+        value |= (uint64_t)buffer[i] << (i * 8);
     }
-    return (size_t)size;
+    return (size_t)value;
 }
 
-size_t _read_bytes(FILE *file, const size_t quantity){
+size_t read_bytes_to_value(FILE *file, const size_t quantity){
     uint8_t bytes[quantity];
-    read_bytes(file, bytes, quantity);
+    read_bytes_to_buffer(file, bytes, quantity);
     size_t value = translate_bytes(bytes, quantity);
     return value;
 }
 
-void print_bytes(uint8_t *buff, const size_t buff_cap){
-    for (size_t i = 0; i < buff_cap; ++i){
-        printf("%u ", buff[i]);
+void print_bytes(uint8_t *buffer, const size_t buffer_capacity){
+    for (size_t i = 0; i < buffer_capacity; ++i){
+        printf("%u ", buffer[i]);
     }
     printf("\n");
 }
 
-void print_date(uint8_t *buff, const size_t buff_cap){
-    for (size_t i = 0; i < buff_cap; ++i){
-        if (buff[i] < 10 && i != 0 && i != 1){
-            printf("0%u", buff[i]);
+void print_date(uint8_t *buffer, const size_t buffer_capacity){
+    for (size_t i = 0; i < buffer_capacity; ++i){
+        if (buffer[i] < 10 && i != 0 && i != 1){
+            printf("0%u", buffer[i]);
         } else {
-            printf("%u", buff[i]);
+            printf("%u", buffer[i]);
         }
 
         if (i == 4){
@@ -109,19 +109,19 @@ void print_date(uint8_t *buff, const size_t buff_cap){
     printf("\n");
 }
 
-void print_ascii(uint8_t *buff, const size_t buff_cap){
-    for (size_t i = 0; i < buff_cap; ++i){
-        printf("%c", (char)buff[i]);
+void print_ascii(uint8_t *buffer, const size_t buffer_capacity){
+    for (size_t i = 0; i < buffer_capacity; ++i){
+        printf("%c", (char)buffer[i]);
     }
     printf("\n");
 }
 
-void print_tags(uint8_t *buff, const size_t buff_cap){
+void print_tags(uint8_t *buffer, const size_t buffer_capacity){
     printf("#");
-    for (size_t i = 0; i < buff_cap; ++i){
-        if (buff[i] == 0 && i != buff_cap - 1){
+    for (size_t i = 0; i < buffer_capacity; ++i){
+        if (buffer[i] == 0 && i != buffer_capacity - 1){
             printf(" #");
-        } else { printf("%c", (char)buff[i]); }
+        } else { printf("%c", (char)buffer[i]); }
     }
     printf("\n");
 }
@@ -130,10 +130,10 @@ void print_tags(uint8_t *buff, const size_t buff_cap){
 #define CAP 8
 #define ANM 8
 const uint8_t magic_caff[MGC] = {67, 65, 70, 70};
-size_t read_caff_hdr(FILE *file){
+size_t read_caff_header(FILE *file){
     // MAGIC
     uint8_t magic[MGC];
-    read_bytes(file, magic, MGC);
+    read_bytes_to_buffer(file, magic, MGC);
     if (memcmp(magic_caff, magic, MGC) != 0){
         fprintf(stderr,
                 "%sERROR%s: file has unknown magic in a block: %c %c %c %c\n",
@@ -149,21 +149,21 @@ size_t read_caff_hdr(FILE *file){
     /* header length is also predefined
     so there is no need to use this
     information, just read it to confirm */
-    const size_t size = _read_bytes(file, CAP);
-    assert(size == 20);
+    const size_t header_size = read_bytes_to_value(file, CAP);
+    assert(header_size == 20);
 
     // ANIMATIONS
     /* not capacity but still a size value
     and still ocupies 8-bytes */
-    const size_t anim = _read_bytes(file, ANM);
+    const size_t number_of_animations = read_bytes_to_value(file, ANM);
 #if LOG
-    printf("number of animations: %zu\n", anim);
+    printf("number of animations: %zu\n", number_of_animations);
 #endif
-    return anim;
+    return number_of_animations;
 }
 
 #define DTE 6
-void read_caff_crd(FILE *file){
+void read_caff_credits(FILE *file){
     // DATE
     /* Y - year (2 bytes)
        M - month (1 byte)
@@ -171,30 +171,30 @@ void read_caff_crd(FILE *file){
        h - hour (1 byte)
        m - minute (1 byte2) */
     uint8_t date[DTE];
-    read_bytes(file, date, DTE);
+    read_bytes_to_buffer(file, date, DTE);
 #if LOG
     printf("date: ");
     print_date(date, DTE);
 #endif
     // CREATOR
-    const size_t s_creator = _read_bytes(file, CAP);
-    if (s_creator == 0){
+    const size_t size_of_creator = read_bytes_to_value(file, CAP);
+    if (size_of_creator == 0){
         printf("%sWARNING%s: file does not define the creator\n",
                 WARN_SET, RESET);
     } else {
-        uint8_t creator[s_creator];
-        read_bytes(file, creator, s_creator);
+        uint8_t creator[size_of_creator];
+        read_bytes_to_buffer(file, creator, size_of_creator);
 #if LOG
         printf("creator: ");
-        print_ascii(creator, s_creator);
+        print_ascii(creator, size_of_creator);
 #endif
     }
 }
 
-void create_jpg(uint8_t *rgb_pixels, size_t s_pixels, size_t width, size_t height){
+void create_jpg(uint8_t *rgb_pixels, size_t width, size_t height){
     int quality = 99;
     int stride_in_bytes = 3 * width;
-    int write = stbi_write_jpg(file_name, width, height, 3, rgb_pixels, quality);
+    int write = stbi_write_jpg(g_file_name, width, height, 3, rgb_pixels, quality);
     if (write == 0) {
         fprintf(stderr,
                 "%sERROR%s: output file could not be written\n",
@@ -218,7 +218,7 @@ void create_jpg(uint8_t *rgb_pixels, size_t s_pixels, size_t width, size_t heigh
     }
     assert(write == 1);
 #if LOG
-    printf("successfully saved to \"%s\"\n", file_name);
+    printf("successfully saved to \"%s\"\n", g_file_name);
 #endif
 }
 
@@ -229,7 +229,7 @@ const uint8_t magic_ciff[MGC] = {67, 73, 70, 70};
 void read_ciff(FILE *file, bool save){
     // MAGIC
     uint8_t magic[MGC];
-    read_bytes(file, magic, MGC);
+    read_bytes_to_buffer(file, magic, MGC);
     if (memcmp(magic_ciff, magic, MGC) != 0){
         fprintf(stderr,
                 "%sERROR%s: file has unknown magic in a block: %c %c %c %c\n",
@@ -242,43 +242,43 @@ void read_ciff(FILE *file, bool save){
     }
 
     // HEADER SIZE
-    const size_t s_header = _read_bytes(file, CAP);
+    const size_t header_size = read_bytes_to_value(file, CAP);
 
     /* 8-byte long integer,
     its value is the size of the image
 	pixels located at the end of the file.
     Its value must be width*heigth*3 */
     // CONTENT SIZE
-    size_t s_pixels = _read_bytes(file, CAP);
+    size_t pixel_size = read_bytes_to_value(file, CAP);
     // WIDTH
-    size_t s_width = _read_bytes(file, WDT);
+    size_t width_size = read_bytes_to_value(file, WDT);
     // HEIGHT
-    size_t s_height = _read_bytes(file, HGT);
+    size_t height_size = read_bytes_to_value(file, HGT);
 
-    if (s_pixels != (s_width * s_height * 3)){
+    if (pixel_size != (width_size * height_size * 3)){
         fprintf(stderr,
                 "%sERROR%s: pixel size is not equal to size defined in header\n",
                 ERR_SET, RESET);
         exit(-1);
     }
-    assert(s_pixels == (s_width * s_height * 3));
+    assert(pixel_size == (width_size * height_size * 3));
 
     // bytes for caption and tags
-    const size_t s_caption_tags = s_header - MGC    // 4-byte magic
-                                           - CAP      // 8-byte header size
-                                           - CAP      // 8-byte content size
-                                           - WDT    // 8-byte width of image
-                                           - HGT;  // 8-byte height of image
+    const size_t caption_tags_size = header_size - MGC    // 4-byte magic
+                                                 - CAP      // 8-byte header size
+                                                 - CAP      // 8-byte content size
+                                                 - WDT    // 8-byte width of image
+                                                 - HGT;  // 8-byte height of image
 
     // CAPTION
-    uint8_t t_caption[s_caption_tags];
+    uint8_t caption_temp[caption_tags_size];
     size_t iter = 0;
-    uint8_t buff[1];
-    read_bytes(file, buff, 1);
-    while (buff[0] != ESC) {
-        t_caption[iter++] = buff[0];
-        read_bytes(file, buff, 1);
-        if (iter == s_caption_tags){
+    uint8_t buffer[1];
+    read_bytes_to_buffer(file, buffer, 1);
+    while (buffer[0] != ESC) {
+        caption_temp[iter++] = buffer[0];
+        read_bytes_to_buffer(file, buffer, 1);
+        if (iter == caption_tags_size){
             fprintf(stderr,
                     "%sERROR%s: file caption larger than what header defines\n",
                     ERR_SET, RESET);
@@ -286,31 +286,31 @@ void read_ciff(FILE *file, bool save){
         }
     }
     // escaped caption
-    size_t s_caption = iter + 1;
+    size_t caption_size = iter + 1;
     if (iter == 0){
         printf("%sWARNING%s: file does not define the caption\n",
                 WARN_SET, RESET);
     } else {
-        uint8_t caption[s_caption];
+        uint8_t caption[caption_size];
         // copy data from temp caption
-        for (int i = 0; i < s_caption; ++i){
-            caption[i] = t_caption[i];
+        for (unsigned int i = 0; i < caption_size; ++i){
+            caption[i] = caption_temp[i];
         }
 #if LOG
         printf("caption: ");
-        print_ascii(caption, s_caption);
+        print_ascii(caption, caption_size);
 #endif
     }
 
     // TAGS
-    size_t s_tags = s_caption_tags - s_caption;
-    if (s_tags == 0){
+    size_t tags_size = caption_tags_size - caption_size;
+    if (tags_size == 0){
         printf("%sWARNING%s: file does not include any tags\n",
                 WARN_SET, RESET);
     } else {
-        uint8_t tags[s_tags];
-        read_bytes(file, tags, s_tags);
-        for (size_t i = 0; i < s_tags; ++i){
+        uint8_t tags[tags_size];
+        read_bytes_to_buffer(file, tags, tags_size);
+        for (size_t i = 0; i < tags_size; ++i){
             if (tags[i] == ESC){
                 fprintf(stderr,
                         "%sERROR%s: file contains escape ASCII in tags\n",
@@ -320,25 +320,25 @@ void read_ciff(FILE *file, bool save){
         }
 #if LOG
         printf("tags: ");
-        print_tags(tags, s_tags);
+        print_tags(tags, tags_size);
 #endif
     }
 
     // PIXELS
-    if (s_pixels == 0){
+    if (pixel_size == 0){
         printf("%sWARNING%s: file is missing the pixel data\n",
                 WARN_SET, RESET);
     } else {
-        uint8_t pixels[s_pixels];
-        read_bytes(file, pixels, s_pixels);
-        if (save){ create_jpg(pixels, s_pixels, s_width, s_height); }
+        uint8_t pixels[pixel_size];
+        read_bytes_to_buffer(file, pixels, pixel_size);
+        if (save){ create_jpg(pixels, width_size, height_size); }
     }
 }
 
 #define DUR 8
-void read_caff_anm(FILE *file, bool save){
+void read_caff_animation(FILE *file, bool save){
     // DURATION
-    size_t duration = _read_bytes(file, DUR);
+    size_t duration = read_bytes_to_value(file, DUR);
 # if LOG
     printf("duration: %zu\n", duration);
 #endif
@@ -350,25 +350,25 @@ void read_caff_anm(FILE *file, bool save){
 #define SZ 8
 void read_caff(FILE *file){
     // HEADER
-    size_t h_id = _read_bytes(file, ID);
-    if (h_id != 1){
+    size_t header_id = read_bytes_to_value(file, ID);
+    if (header_id != 1){
         fprintf(stderr,
                 "%sERROR%s: file does not start with a header\n",
                 ERR_SET, RESET);
         exit(-1);
     }
-    assert(h_id == 1);
+    assert(header_id == 1);
 
     /* cap is no needed for hdr
     because all chunks lengths
     are predefined in the format */
-    const size_t h_length = _read_bytes(file, SZ);
-    assert(h_length == 20);
+    const size_t header_size = read_bytes_to_value(file, SZ);
+    assert(header_size == 20);
 
     /* the only valuable information
     in the header block is the
     number of animations in the CAFF */
-    const size_t anim = read_caff_hdr(file);
+    const size_t number_of_animations = read_caff_header(file);
 #if LOG
     printf("\n");
 #endif
@@ -377,18 +377,18 @@ void read_caff(FILE *file){
     /* read all blocks from file
     + 1 for the credits block */
     bool save_first = true;
-    for (size_t i = 0; i < anim + 1; ++i){
-        size_t b_id = _read_bytes(file, ID);
-        size_t b_size = _read_bytes(file, SZ);
-        if (b_id == 2){
+    for (size_t i = 0; i < number_of_animations + 1; ++i){
+        size_t block_id = read_bytes_to_value(file, ID);
+        size_t block_size = read_bytes_to_value(file, SZ);
+        if (block_id == 2){
             // CREDITS
-            read_caff_crd(file);
+            read_caff_credits(file);
 #if LOG
             printf("\n");
 #endif
-        } else if (b_id == 3){
+        } else if (block_id == 3){
             // ANIMATION
-            read_caff_anm(file, save_first);
+            read_caff_animation(file, save_first);
             save_first = false;
 #if LOG
             printf("\n");
@@ -396,7 +396,7 @@ void read_caff(FILE *file){
         } else {
             fprintf(stderr,
                     "%sERROR%s: file has unknown id in a block: %zu\n",
-                    ERR_SET, RESET, b_id);
+                    ERR_SET, RESET, block_id);
             exit(-1);
         }
     }
@@ -439,14 +439,14 @@ int main(int argc, char const *argv[])
         exit(-1);
     }
 
-    file_name = _file_name(file_path);
-    if (file_name == NULL){
+    set_g_file_name(file_path);
+    if (g_file_name == NULL){
         fprintf(stderr,
             "%sERROR%s: filename was not provided\n", ERR_SET, RESET);
         usage(stderr, program);
         exit(-1);
     }
-    assert(file_name != NULL);
+    assert(g_file_name != NULL);
 
     // check for overflow
     if (*argv != NULL){
